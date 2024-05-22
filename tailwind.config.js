f663271c3b3daecb590552e0975621460292a82e
songyi225 <sy920225@naver.com>
2024-05-22 14:40:03 +0900

    tailwind config 셋팅 실습

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,21 @@
 module.exports = {
   content: ["./src/**/*.html"],
   theme: {
-    extend: {},
+    fontFamily: {
+      suit: ['SUIT Variable', 'sans-serif'],
+    },
+    screens: {
+      'sm': '480px',
+      'md': '640px',
+      'lg': '768px',
+      'xl': '1024px',
+    },
+    extend: {
+    },
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/aspect-ratio'),
+  ],
 }
 
