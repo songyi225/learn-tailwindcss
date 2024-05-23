db50d780439ad71417457880539aeb641aa57f1d
songyi225 <sy920225@naver.com>
2024-05-23 10:43:52 +0900

    네이버 로그인 구현 완료

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,31 +3,28 @@ module.exports = {
   content: ["./src/**/*.html"],
   theme: {
     fontFamily: {
-      suit: ['SUIT Variable', 'sans-serif'],
-    },
-    screens: {
-      'sm': '480px',
-      'md': '640px',
-      'lg': '768px',
-      'xl': '1024px',
+      suit: ["SUIT Variable", "sans-serif"],
     },
     extend: {
-      "colors": {
-        "naverGreen": "#03cf5d",
-        "white": "#ffffff",
-        "sliver": "#999999",
-        "gray":"#e9f0fd",
-        "sliverLight": "#dadada",
-        "warningRed": "#ff1414",
-        "silverDark": "#666666",
-        "blackAlpha10": "#000000",
-        "blackDark": "#333333"
-       },
+      colors: {
+        primary: "#03cf5d",
+        white: "#ffffff",
+        warning: "#ff1414",
+        "blue-10": "#e9f0fd",
+        "gray-10": "#dadada",
+        "gray-20": "#999999",
+        "gray-30": "#666666",
+        "gray-40": "#333333",
+        black: "#121212",
+      },
+      backgroundImage: {
+        "gradient-radial":
+          "radial-gradient(circle, var(--tw-gradient-from), var(--tw-gradient-to))",
+      },
     },
   },
   plugins: [
     // require('@tailwindcss/forms'),
-    require('@tailwindcss/aspect-ratio'),
+    require("@tailwindcss/aspect-ratio"),
   ],
-}
-
+};
